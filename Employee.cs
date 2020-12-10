5c67120bdaca246509c75cbbc2f819b6045ff8fd
Ethan De Guzman <S00199053@mail.itsligo.ie>
2020-12-10 13:39:13 +0000

    Worked through 70% of CA

---
+++ Employee.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    public abstract class Employee
+    {
+        protected string FirstName;
+        protected string Surname;
+
+        public string firstname { get { return FirstName; } }
+        public string surname { get { return Surname; } }
+        public Employee(string FirstName, string Surname)
+        {
+            this.FirstName = FirstName;
+            this.Surname = Surname;
+        }
+
+        public abstract decimal CalculateMonthlyPay();
+
+        public abstract override string ToString();
+
+    }
+
+    public class FulltimeEmployee : Employee
+    {
+        private decimal Salary;
+        public decimal salary 
+        {
+            get { return Salary; } 
+            set { Salary = value; }
+        }
+
+        public FulltimeEmployee(string FirstName, string Surname, decimal salary) : base(FirstName, Surname)
+        {
+            this.salary = salary;
+        }
+
+        public override decimal CalculateMonthlyPay()
+        {
+            return salary / 12;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Surname.ToUpper()}, {FirstName} - Full Time");
+        }
+    }
+
+    public class ParttimeEmployee : Employee
+    {
+        private decimal HourlyRate;
+        private double HoursWorked;
+        public decimal hourlyrate
+        {
+            get { return HourlyRate; }
+            set { HourlyRate = value; }
+        }
+
+        public double hoursworked
+        {
+            get { return HoursWorked; }
+            set { HoursWorked = value; }
+        }
+
+        public ParttimeEmployee(string FirstName, string Surname, decimal HourlyRate, double HoursWorked) : base(FirstName, Surname)
+        {
+            this.HourlyRate = HourlyRate;
+            this.HoursWorked = HoursWorked;
+        }
+
+        public override decimal CalculateMonthlyPay()
+        {
+            return HourlyRate * (decimal) HoursWorked;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Surname.ToUpper()}, {FirstName} - Part Time");
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,98 @@ namespace CA2
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Employee> Employees = new ObservableCollection<Employee>();
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FulltimeEmployee FtEmployee1 = new FulltimeEmployee("Ethan", "De Guzman", 500);
+            FulltimeEmployee FtEmployee2 = new FulltimeEmployee("Ethan", "De Guzman", 300);
+            ParttimeEmployee PtEmployee1 = new ParttimeEmployee("Ethan", "De Guzman", 5, 2);
+            ParttimeEmployee PtEmployee2 = new ParttimeEmployee("Ethan", "De Guzman", 4, 3);
+
+            Employees.Add(FtEmployee1);
+            Employees.Add(FtEmployee2);
+
+            Employees.Add(PtEmployee1);
+            Employees.Add(PtEmployee2);
+
+            lstEmployee.ItemsSource = Employees;
+
+        }
+
+        private void lstEmployee_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (chkFullTime.IsChecked == true)
+            {
+                FulltimeEmployee selectedEmployee = lstEmployee.SelectedItem as FulltimeEmployee;
+                if (selectedEmployee != null)
+                {
+                    tbxFirstName.Text = selectedEmployee.firstname;
+                    tbxSurname.Text = selectedEmployee.surname;
+                    tbxSalary.Text = Convert.ToString(selectedEmployee.salary);
+                    btnFT.IsChecked = true;
+                    tblkMonthlyPay.Text = selectedEmployee.CalculateMonthlyPay().ToString("€0.##");
+
+                    tbxHoursWorked.Clear();
+                    tbxHourlyRate.Clear();
+                }
+            }
+            if (chkPartTime.IsChecked == true)
+            {
+                ParttimeEmployee selectedEmployee = lstEmployee.SelectedItem as ParttimeEmployee;
+                if (selectedEmployee != null)
+                {
+                    tbxFirstName.Text = selectedEmployee.firstname;
+                    tbxSurname.Text = selectedEmployee.surname;
+                    tbxHoursWorked.Text = Convert.ToString(selectedEmployee.hoursworked);
+                    tbxHourlyRate.Text = Convert.ToString(selectedEmployee.hourlyrate);
+                    btnPT.IsChecked = false;
+                    tblkMonthlyPay.Text = selectedEmployee.CalculateMonthlyPay().ToString("€0.##");
+
+                    tbxSalary.Clear();
+                }
+            }
+
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string firstname = tbxFirstName.Text;
+            string surname = tbxSurname.Text;
+            
+            if (btnFT.IsChecked == true)
+            {
+                decimal salary = decimal.Parse(tbxSalary.Text);
+                FulltimeEmployee FtEmployee = new FulltimeEmployee(firstname, surname, salary);
+                tblkMonthlyPay.Text = FtEmployee.CalculateMonthlyPay().ToString("€0.##");
+                Employees.Add(FtEmployee);
+            }
+            else
+            {
+                double hoursworked = double.Parse(tbxHoursWorked.Text);
+                decimal hourlyrate = decimal.Parse(tbxHourlyRate.Text);
+                ParttimeEmployee PtEmployee = new ParttimeEmployee(firstname, surname, hourlyrate, hoursworked);
+                tblkMonthlyPay.Text = PtEmployee.CalculateMonthlyPay().ToString("€0.##");
+                Employees.Add(PtEmployee);
+            }
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbxFirstName.Clear();
+            tbxSurname.Clear();
+            tbxSalary.Clear();
+            tbxHoursWorked.Clear();
+            tbxHourlyRate.Clear();
+            btnFT.IsChecked = false;
+            btnPT.IsChecked = false;
+            tblkMonthlyPay.Text = "";
+        }
     }
 }
